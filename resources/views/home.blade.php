b8486fceb866c05bc3048f66089cc1bc3dc37d8f
yevmoga <yevmoga@gmail.com>
2017-03-24 15:29:53 +0200

    header and footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,50 +1,144 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-<head>
-    <meta charset="utf-8">
-    <title>@yield('title')</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- Bootstrap -->
-    <link href="{{URL::asset('fornax/css/bootstrap.css')}}" rel="stylesheet">
-    <link href="{{URL::asset('fornax/css/bootstrap-responsive.css')}}" rel="stylesheet">
-    <link href="{{URL::asset('fornax/css/style.css')}}" rel="stylesheet">
-    {{--<link rel="stylesheet" type="text/css" href="{{URL::asset('/css/app.css')}}">--}}
-    <link rel="stylesheet" type="text/css" href="{{URL::asset('/css/styles.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{URL::asset('/css/bootstrap-datetimepicker.min.css')}}">
+@extends('index')
+
+@section('title', 'Calendar')
+
+@section('head')
+
+@endsection
+
+@section('content')
+
+    <div class="container">
+
+        <!--Carousel
+        ==================================================-->
+
+        <div id="myCarousel" class="carousel slide">
+            <div class="carousel-inner">
+
+                <div class="active item">
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="span6">
+
+                                <div class="carousel-caption">
+                                    <h1>Example headline</h1>
+                                    <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec
+                                        id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies
+                                        vehicula ut id elit.</p>
+                                    <a class="btn btn-large btn-primary" href="#">Sign up today</a>
+                                </div>
+
+                            </div>
+
+                            <div class="span6"><img src="{{URL::asset('fornax/img/slide/slide1.jpg')}}"></div>
+
+                        </div>
+                    </div>
 
 
-    <!--Font-->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600' rel='stylesheet' type='text/css'>
+                </div>
+
+                <div class="item">
+
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="span6">
+
+                                <div class="carousel-caption">
+                                    <h1>Example headline</h1>
+                                    <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec
+                                        id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies
+                                        vehicula ut id elit.</p>
+                                    <a class="btn btn-large btn-primary" href="#">Sign up today</a>
+                                </div>
+
+                            </div>
+
+                            <div class="span6"><img src="{{URL::asset('fornax/img/slide/slide2.jpg')}}"></div>
+
+                        </div>
+                    </div>
+
+                </div>
 
 
+            </div>
+            <!-- Carousel nav -->
+            <a class="carousel-control left " href="#myCarousel" data-slide="prev"><i class="icon-chevron-left"></i></a>
+            <a class="carousel-control right" href="#myCarousel" data-slide="next"><i
+                        class="icon-chevron-right"></i></a>
+            <!-- /.Carousel nav -->
+
+        </div>
+        <!-- /Carousel -->
 
 
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+        <!-- Feature
+          ==============================================-->
 
 
+        <div class="row feature-box">
+            <div class="span12 cnt-title">
+                <h1>At vero eos et accusamus et iusto odio dignissimos</h1>
+                <span>Contrary to popular belief, Lorem Ipsum is not simply random text.</span>
+            </div>
+
+            <div class="span4">
+                <img src="{{URL::asset('fornax/img/icon3.png')}}">
+                <h2>Feature A</h2>
+                <p>
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                </p>
+
+                <a href="#">Read More &rarr;</a>
+
+            </div>
+
+            <div class="span4">
+                <img src="{{URL::asset('fornax/img/icon2.png')}}">
+                <h2>Feature B</h2>
+                <p>
+                    Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                </p>
+                <a href="#">Read More &rarr;</a>
+            </div>
+
+            <div class="span4">
+                <img src="{{URL::asset('fornax/img/icon1.png')}}">
+                <h2>Feature C</h2>
+                <p>
+                    Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+                </p>
+                <a href="#">Read More &rarr;</a>
+            </div>
+        </div>
 
 
+        <!-- /.Feature -->
+
+        <div class="hr-divider"></div>
+
+        <!-- Row View -->
 
 
-    <!-- SCRIPT
-    ============================================================-->
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="{{URL::asset('fornax/js/bootstrap.min.js')}}"></script>
+        <div class="row">
+            <div class="span6"><img src="{{URL::asset('fornax/img/responsive.png')}}"></div>
 
-    <script src="{{URL::asset('/js/app.js')}}"></script>
-    <script src="{{URL::asset('/js/bootstrap-datetimepicker.min.js')}}"></script>
-    <script>    var BASE_URL = "{{ url('/') }}" </script>
-    <script src="{{URL::asset('/js/main.js')}}"></script>
-</head>
-<body>
+            <div class="span6">
+                <img class="hidden-phone" src="{{URL::asset('fornax/img/icon4.png')}}" alt="">
+                <h1>Fully Responsive</h1>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                    Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+                    amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                <a href="#">Read More &rarr;</a>
+            </div>
+        </div>
 
-    @yield('content')
 
-</body>
-</html>
+    </div>
+    <!-- /.Row View -->
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,34 +1,45 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Bootstrap -->
+    <link href="{{URL::asset('fornax/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('fornax/css/bootstrap-responsive.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('fornax/css/style.css')}}" rel="stylesheet">
+    {{--<link rel="stylesheet" type="text/css" href="{{URL::asset('/css/app.css')}}">--}}
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('/css/styles.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('/css/bootstrap-datetimepicker.min.css')}}">
 
-        <title>@yield('title')</title>
+    <!--Font-->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600' rel='stylesheet' type='text/css'>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 
-        <!-- Styles -->
-        <link rel="stylesheet" type="text/css" href="{{URL::asset('/css/app.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{URL::asset('/css/bootstrap-datetimepicker.min.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{URL::asset('/css/styles.css')}}">
+    <!-- SCRIPT
+    ============================================================-->
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="{{URL::asset('fornax/js/bootstrap.min.js')}}"></script>
 
-        <script src="{{URL::asset('/js/jquery-3.1.1.min.js')}}"></script>
-        <script src="{{URL::asset('/js/app.js')}}"></script>
-        <script src="{{URL::asset('/js/bootstrap-datetimepicker.min.js')}}"></script>
-        <script>    var BASE_URL = "{{ url('/') }}" </script>
-        <script src="{{URL::asset('/js/main.js')}}"></script>
+    <script src="{{URL::asset('/js/app.js')}}"></script>
+    <script src="{{URL::asset('/js/bootstrap-datetimepicker.min.js')}}"></script>
+    <script>    var BASE_URL = "{{ url('/') }}" </script>
+    <script src="{{URL::asset('/js/main.js')}}"></script>
+</head>
+<body>
 
-        @yield('head')
+    @include('layouts.header')
 
-    </head>
-    <body>
+    @yield('content')
 
-    <div class="container">
-        @yield('content')
-    </div>
+    @include('layouts.footer')
 
-    </body>
+</body>
 </html>
