618e324a1e31a81300e5564292910e7a2d557c40
yevmoga <yevmoga@gmail.com>
2017-03-25 10:59:59 +0200

    calendar event drop

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -3,7 +3,6 @@
  */
 
 
-
 /**
  * Отрисовка календаря
  *
@@ -36,6 +35,16 @@ function getCalendar(calendarData) {
         },
         firstDay: '1',
         events: calendarData,
+        eventDrop: function (event, delta, revertFunc) {
+            editTask(event.id,
+                {
+                    'id': event.id,
+                    'date': event.start.format(),
+                    'title': event.source.events[0].title,
+                    'description': event.source.events[0].description,
+                    'status': event.source.events[0].status
+                });
+        }
     });
 }
 
@@ -56,12 +65,12 @@ function getEvents() {
 
 /**
  * Вызов попапа при создании или обновлении таска
- * 
+ *
  * @param id
  */
 function createPopup(id) {
     var url;
-    if(typeof id == 'number') {
+    if (typeof id == 'number') {
         url = BASE_URL + '/calendar/' + id;
     } else {
         url = BASE_URL + '/calendar/create/';
@@ -102,11 +111,14 @@ function saveTask() {
 /**
  * Обновление существующего таска
  */
-function editTask() {
-    var data = $('#add-task-form').serializeObject();
+function editTask(id, data) {
+    if (id === undefined) {
+        var data = $('#add-task-form').serializeObject();
+        var id = data.id;
+    }
     $.ajax({
         method: 'put',
-        url: BASE_URL + '/calendar/' + data.id,
+        url: BASE_URL + '/calendar/' + id,
         data: data,
         dataType: 'json',
         success: function (data) {
@@ -137,7 +149,9 @@ $(function () {
         getEvents();
         $(document).on("click", '.btn-add-event', createPopup);
         $(document).on("click", '.save-task', saveTask);
-        $(document).on("click", '.edit-task', editTask);
+        $(document).on("click", '.edit-task', function () {
+            editTask()
+        });
         $(document).on("click", '.remove', removeTask);
     });
 });
