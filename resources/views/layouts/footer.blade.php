4db0bcfef65403f3fcfd15874f4ca3d55c84f2f3
yevmoga <yevmoga@gmail.com>
2017-03-26 16:52:22 +0300

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,13 +5,13 @@
     <div class="container">
         <div class="row">
             <div class="span6">Copyright &copy {{date('Y')}} planmee | All Rights Reserved  <br>
-                {{--<small>Aliquam tincidunt mauris eu risus. - добавить слоган</small>--}}
+                <small>По любым вопросам или предложениям обращайтесь к <a href="https://vk.com/moga.yevgen" style="color: grey">Жене</a> или <a href="https://vk.com/id56171232" style="color: grey">Андрею</a></small>
             </div>
             <div class="span6">
                 <div class="social pull-right">
-                    <a href="#"><img src="{{URL::asset('fornax/img/social/googleplus.png')}}" alt=""></a>
+                    {{--<a href="#"><img src="{{URL::asset('fornax/img/social/googleplus.png')}}" alt=""></a>--}}
                     {{--<a href="#"><img src="{{URL::asset('fornax/img/social/dribbble.png')}}" alt=""></a>--}}
-                    <a href="#"><img src="{{URL::asset('fornax/img/social/twitter.png')}}" alt=""></a>
+{{--                    <a href="#"><img src="{{URL::asset('fornax/img/social/twitter.png')}}" alt=""></a>--}}
                     {{--<a href="#"><img src="{{URL::asset('fornax/img/social/dribbble.png')}}" alt=""></a>--}}
                     {{--<a href="#"><img src="{{URL::asset('fornax/img/social/rss.png')}}" alt=""></a>--}}
                 </div>
