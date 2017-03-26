e191ce03f2254823e73f261c72d0066f981cc91f
yevmoga <yevmoga@gmail.com>
2017-03-26 16:40:46 +0300

    футер и куда ведет лого

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,17 +4,24 @@
     <div class="container">
         <div class="row">
             <!--LOGO-->
-            <div class="span3"><a class="brand" href="#">
-                    <img src="{{URL::asset('fornax/img/logo.png')}}"/>
-                    {{--<h2>planmee</h2>--}}
-                </a></div>
+            <div class="span3">
+                @if(auth()->check())
+                    <a class="brand" href="{{URL('/calendar')}}">
+                @else
+                    <a class="brand" href="{{URL('/')}}">
+                @endif
+                        <img src="{{URL::asset('fornax/img/logo.png')}}"/>
+                        {{--<h2>planmee</h2>--}}
+                    </a>
+            </div>
             <!-- /LOGO -->
 
             <!-- MAIN NAVIGATION -->
             <div class="span9">
                 <div class="navbar  pull-right">
                     <div class="navbar-inner">
-                        <a data-target=".navbar-responsive-collapse" data-toggle="collapse" class="btn btn-navbar"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a>
+                        <a data-target=".navbar-responsive-collapse" data-toggle="collapse" class="btn btn-navbar"><span
+                                    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></a>
                         <div class="nav-collapse collapse navbar-responsive-collapse">
                             <ul class="nav">
                                 @if(auth()->check())
